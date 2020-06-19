8511500a8013c85c6479724bc9dff46ee50fcf15
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-19 02:46:45 +0000

    주소 넘겨서 받기

--- main.c
+++ main.c
@@ -4,30 +4,56 @@
 
 #include <stdio.h>
 
+
+void change(int* a){
+
+  *a = 50;
+}
+
+
+
 int main(void) {
 
-  int a = 1;
-  int b = 5;
+  // int a = 1;
+  // int b = 5;
 
-  printf("== 변수의 주소 ==\n");
-  printf("&a : %ld\n", (long)&a);
-  printf("&b : %ld\n", (long)&b);
+  // printf("== 변수의 주소 ==\n");
+  // printf("&a : %ld\n", (long)&a);
+  // printf("&b : %ld\n", (long)&b);
 
-  // 수정가능지역 시작
-  int *p = &a;
-  *p = 2;
-  int *u = (&a - 1); //1 은 1이아니라 int는 4바이트를 가지므로 주소에서 4가 빠짐.
-  *u = 10;
-  printf("p : %ld\n", (long)p);
-  // 수정가능지역 끝
+  // // 수정가능지역 시작
+  // int *p = &a;
+  // *p = 2;
+  // int *u = (&a - 1); //1 은 1이아니라 int는 4바이트를 가지므로 주소에서 4가 빠짐.
+  // *u = 10;
+  // printf("p : %ld\n", (long)p);
+  // // 수정가능지역 끝
 
 
 
-  printf("== 변수의 값 ==\n");
-  printf("a : %d\n", a);
-  // 출력 => a : 2
-  printf("b : %d\n", b);
-  // 출력 => b : 10
+  // printf("== 변수의 값 ==\n");
+  // printf("a : %d\n", a);
+  // // 출력 => a : 2
+  // printf("b : %d\n", b);
+  // // 출력 => b : 10
   
+
+
+  // 문제 : 원본값을 훼손하는 change 함수를 만들어주세요.
+
+  int x = 20;
+  printf("x의 주소값 : %ld\n", (long)&x);
+
+  printf("change 함수 호출하기 전의 x : %d\n", x);
+
+
+  change(&x); //이부분만 수정 가능
+
+  
+  
+  printf("change 함수 호출한 후의 x : %d\n", x);
+  // 출력 => change 함수 호출한 후의 x : 50
+
+
   return 0;
 }
